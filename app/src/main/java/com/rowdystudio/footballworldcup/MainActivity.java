759f68f62775544a45d140fb9ccacaf831f1367b
Nahian <nahian3832@gmail.com>
2022-11-19 12:10:31 +0600

    3 Different Fragment added to handle task Minor Features Added

--- app/src/main/java/com/rowdystudio/footballworldcup/MainActivity.java
+++ app/src/main/java/com/rowdystudio/footballworldcup/MainActivity.java
@@ -2,11 +2,18 @@ package com.rowdystudio.footballworldcup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.rowdystudio.footballworldcup.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,53 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
+        // again binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // default fragment to show first time landing page
+        replaceFragment(new HomeFragment());
+
+        binding.bottomnavBar.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.matches:
+                    replaceFragment(new MatchesFragment());
+                    break;
+
+                case R.id.table:
+                    replaceFragment(new StandingsFragment());
+                    break;
+
+            }
+
+
+            return true;
+        });
+
+
+
     }
+
+
+
+    // fragment transition  commit
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+
+
+
 }
