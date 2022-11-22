2c68909db42a13883a344e0580adb1b83b433cdd
Nahian <nahian3832@gmail.com>
2022-11-22 21:34:11 +0600

    Design Fixed

--- app/src/main/java/com/rowdystudio/footballworldcup/MainActivity.java
+++ app/src/main/java/com/rowdystudio/footballworldcup/MainActivity.java
@@ -6,20 +6,25 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.rowdystudio.footballworldcup.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     AdView adView;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
         // support actionbar hide
         getSupportActionBar().hide();
+
+
+        frameLayout = findViewById(R.id.frameLayout);
 
 
         // again binding
